908681a8378f2bc8bffd18567a0f934f740ab178
malex1306 <alexandremarcel350@gmail.com>
2025-05-04 15:47:25 +0200

    Layout

--- DevLiftNew/obj/Debug/net8.0/DevLiftNew.AssemblyInfo.cs
+++ DevLiftNew/obj/Debug/net8.0/DevLiftNew.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevLiftNew")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20739934f5b44e1e4f17c54e7cdee3370f7f6f56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b137adf1997ffc30cbaa607950c953a0ef046541")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevLiftNew")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevLiftNew")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
